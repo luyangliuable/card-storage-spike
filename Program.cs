572bbc2ebb8168c5e9bf49dfb7a3ab40921e9ee9
Liu Luyang <Lucas.Liu@wexinc.com>
2023-02-10 14:01:18 +1100

    ADded some experiment code with Dylan

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Out.WriteLine("This is the miracle");
+        Worker worker1 = new Worker();
+        Worker worker2 = new Worker();
+
+        if (worker1 == worker2) {
+            Console.Out.WriteLine("Equals!");
+        }
+    }
+
+    interface ICanCode<> {
+        
+    }
+
+    interface e {
+        
+    }
+
+    class Worker {
+        public Worker() {
+            ;
+        }
+    }
+}
